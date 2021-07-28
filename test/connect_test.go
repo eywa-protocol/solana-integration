7fb7e4cd06cab950905b1884c8f5caabc1a9bf78
Roman Romanov <rroman4work@gmail.com>
2021-07-28 15:49:36 +0300

    fix

--- test/connect_test.go
+++ test/connect_test.go
@@ -3,9 +3,10 @@ package test
 import (
 	"context"
 	"fmt"
+	"testing"
+
 	"github.com/portto/solana-go-sdk/client"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 var c *client.Client
@@ -13,20 +14,18 @@ var localSolanaUrl string
 
 func init() {
 	c = client.NewClient(client.TestnetRPCEndpoint)
-	localSolanaUrl = "http://127.0.0.1:1024"
+	localSolanaUrl = "http://127.0.0.1:8899"
 }
 
-func Test_testnet_connect(t *testing.T){
+func Test_testnet_connect(t *testing.T) {
 	resp, err := c.GetVersion(context.Background())
 	require.NoError(t, err)
 	fmt.Println("testnet solana version:", resp.SolanaCore)
 }
 
-func Test_local_connect(t *testing.T){
+func Test_local_connect(t *testing.T) {
 	local_client := client.NewClient(localSolanaUrl)
 	resp, err := local_client.GetVersion(context.Background())
 	require.NoError(t, err)
 	fmt.Println("local solana version:", resp.SolanaCore)
 }
-
-
